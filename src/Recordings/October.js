925404f0f8200c6b6af79cf97dc0e4712342057d
Vishmidulanjalee <135816897+Vishmidulanjalee@users.noreply.github.com>
2025-10-23 14:22:44 +0530

    Update October.js

--- src/Recordings/October.js
+++ src/Recordings/October.js
@@ -33,7 +33,7 @@ const October = () => {
       <header className="bg-white shadow">
         <div className="max-w-7xl mx-auto px-4 py-4">
           <h1 className="text-2xl font-bold text-gray-800 font-serif">
-            WEEK TO WOW PROGRAMME — OCTOBER
+            "WEAK TO WOW PROGRAMME" — OCTOBER
           </h1>
         </div>
       </header>
